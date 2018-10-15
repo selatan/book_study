7c471db06dae9dfaf5292a5734d47d868a5a5a63
selatan <353658617@qq.com>
2018-10-15 21:06:06 +0800

    aaa

---
+++ my_book/chapter2.py
@@ -0,0 +1,37 @@
+#变量和简单的数据类型
+
+
+
+message = "Hello Python World"
+print(message)
+
+
+message = "Hello Python World"
+message = "Hello World"
+print(message)
+
+
+name = "ada lovelace"
+print(name.title())     #将字符串每个单词的首写字母改为大写
+print(name.upper())     #将字符串中所有字母改为大写字母
+print(name.lower())     #将字符串中所有字母改为小写字母
+
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + "    "+last_name
+print(full_name)
+
+
+user_name = "huanglijuan"
+a = "Hello,"+user_name+","+"would you like to learn som Python today."
+print(a)
+
+
+#
+# age  = 30
+# print("Happy"+ age +"rd Birthday!")
+
+age  = 30
+print("Happy "+ str(age) +"rd Birthday!")
+
