91e81146fb073d6d3c7b1ed9167104e033986a1c
Administrator <353658617@qq.com>
2018-10-16 13:43:36 +0800

    分

--- my_book/chapter3.py
+++ my_book/chapter3.py
@@ -1,2 +1,19 @@
 #列表简介
-#列表用方括号[] 来表示
+#列表是用方括号[] 来表示的有序集合
+
+
+
+#打印列表
+bicycles = ['ofo','mobike','CCbike','bluebike']
+print(bicycles)
+
+
+#访问列表元素
+print(bicycles[0])   #第一个元素
+print(bicycles[1])   #第二个元素
+print(bicycles[3].title())   #第四个元素
+
+
+
+print(bicycles[-1])   #访问最后一个（倒数第一个）元素
+print(bicycles[-2])   #访问倒数第二个元素，以此类推
