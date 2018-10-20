43ffdd28321460d3505425d8e0ae5047d202c5ac
selatan <353658617@qq.com>
2018-10-20 19:41:15 +0800

    aaa

--- my_book/chapter4.py
+++ my_book/chapter4.py
@@ -27,6 +27,55 @@ for animal in annimals:
 print("The animals would make a great pet!")
 
 
+#range()函数,生成一系列数字,它从第一个数值开始数,并在到达指定的第二个值后停止,因此下面这个循环输出的是1,2,3,4
+for value in range(1,5):
+    print(value)
+
+#使用range()创建数字列表,可以使用函数list()将range()的结果直接转换为列表
+number = list(range(1,6))
+print(number)
+
+#使用range()函数还可以指定步长,下面的代码打印1~10内的偶数
+num = list(range(2,11,2))
+print(num)
+
+#创建一个列表,包含1~10的平方
+squares = []
+for value in range(1,11):
+    square = value**2
+    squares.append(square)      #也可直接写squares.append(value**2)
+print(squares)
 
 
+digits = [1,2,3,4,5,6,7,8,9,0]
+print(min(digits))   #打印列表中最小值
+print(max(digits))   #打印列表中最大值
+print(sum(digits))   #打印列表所有值得和
 
+
+#列表解析
+#使用列表解析创建1~10的平方
+squares1 = [value**2 for value in range(1,11)]
+print(squares1)
+
+
+#P54练习
+#使用for（）循环打印1~20（包含）
+s=[]
+for i in range(1,21):
+    s.append(i)
+print(s)
+
+#创建一个列表，包含1~20中的奇数，再将这些数字打印出来
+s1 = [i for i in range(1,21,2) ]
+print(s1)
+for x in s1:
+    print(x)
+
+#创建一个列表，包含3~30中能被3整除的数字，再将这些数字打印出来
+s2 = [m for m in range(3,31,3)]
+print(s2)
+for n in s2:
+    print(n)
+
+s3 = [value]
