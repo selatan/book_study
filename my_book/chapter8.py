8b3082120ea40cca6db32c500a7845f029b7d731
Administrator <353658617@qq.com>
2018-11-30 21:43:10 +0800

    函数

--- my_book/chapter8.py
+++ my_book/chapter8.py
@@ -67,3 +67,97 @@ describe_city('Tokyo',country='Japan')
 describe_city(country='America',city='Los Angeles')
 
 
+#返回值
+def get_formatted_name(first_name,last_name):
+    full_name =first_name +' '+last_name
+    return full_name.title()
+musician = get_formatted_name('zhang','zan')   #将get_formatted_name()函数的返回值存储在变量musician中
+print(musician)
+
+
+#让实参变成可选的
+#假设需要提供中间名，但不是所有人都有中间名，让中间的参数变成可选的，可以给实参middle_name指定一个默认值-空字符串
+def get_name(first_name,last_name,middle_name=''):         #python将非空字符串解读为True，此时middle_name默认是False
+    if middle_name:            #如果middle_name非空，为true
+        full_name = first_name+' '+middle_name+' '+last_name
+    else:                      #middle_name为false时
+        full_name =first_name+' '+last_name
+    return full_name.title()
+name1 = get_name('zhang','zan')   #没有中间名
+print(name1)
+name2 = get_name('huang','juan','li')         #有中间名
+print(name2)
+
+
+#返回字典-函数可以返回任何类型的值，包括列表和字典等较复杂的数据结构
+def build_person(first_name,last_name):
+    person ={'first':first_name,'last':last_name}
+    return person
+name3 = build_person('zhang','zan')
+print(name3)
+
+
+#返回字典-参数中带有可变参数
+def build_people(first_name,last_name,age =''):
+    person = {'first':first_name,'last':last_name}
+    if age:
+        person['age']=age
+    return person
+name4 = build_people('huang','jie')
+print(name4)
+name5 = build_people('zhang','zan',2)
+print(name5)
+
+
+# #结合while循环-请用户输入姓和名，并打印出问候语
+# def get_full_name(first_name,last_name):
+#     full_name =first_name +' '+last_name
+#     return full_name.title()
+# while True:
+#     print("\nTell me your name: ")
+#     print("(enter 'q' at any time to quit)")
+#     f_name = input("First name : ")
+#     if f_name == 'q':
+#         break
+#
+#     l_name = input("Last name : ")
+#     if l_name =='q':
+#         break
+#     name6 = get_full_name(f_name,l_name)
+#     print("\nHello,"+name6+"!")
+
+
+#8-6
+def city_country(city_name,country):
+    city_info = '"'+city_name+","+country+'"'
+    return city_info
+city1 = city_country('Beijing','China')
+print(city1)
+
+#8-7
+def make_album(singer_name,song_name,songs_num=''):
+    album = {'singer':singer_name,'song':song_name}
+    if songs_num:
+        album['num']=songs_num
+    return album
+album1 = make_album("ESON","L.O.V.E")
+print(album1)
+album2 = make_album("JONGHYUN",'SO GOODBY')
+print(album2)
+album3 = make_album('JAY','J III(EP',6)
+print(album3)
+
+#8-8
+def make_album(singer_name,album_name):
+    album = {'singer':singer_name,'song':album_name}
+    return album
+while True:
+    s_name = input("Please input songer name : ")
+    print("(enter 'q' at any time to quit)")
+    if s_name =='q':
+        break
+    a_name = input("Please input album name : ")
+    if a_name=='q':
+        break
+    info = make_album(s_name,a_name)
+    print(info)
